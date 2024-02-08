0c2fbcc37c1d5f1804e1a60f2315ae8db83f8062
smaldonado1326 <157517882+smaldonado1326@users.noreply.github.com>
2024-02-07 19:22:52 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,15 @@ $(document).ready(function() {
     stop: function() {
       console.log("stopping???");
 
-      let dogeLeft = $(this).position().left; // Use $(this) to refer to the dragged image
+      let dogeLeft = $(this).position().left; 
 
-      // Generate a random target zone within the range of the container width
+      
       let containerWidth = $(".container").width();
-      let targetZoneWidth = 200; // Adjust the width of the target zone as needed
+      let targetZoneWidth = 200; 
       let randomTargetLeft = Math.floor(Math.random() * (containerWidth - targetZoneWidth));
 
       let distance = Math.abs(dogeLeft - randomTargetLeft);
-      let threshold = 200; // Adjust the threshold as needed
+      let threshold = 200; 
 
       if (distance <= threshold) {
         $("#feedback").text("You are hot!");
@@ -29,7 +29,7 @@ $(document).ready(function() {
         $("h4").text("");
 
         $(this).attr("src", "https://www.vobss.com/wp-content/uploads/2022/03/congratulations-meme-vobss-11.jpg");
-        // Change the above line to update the image source as needed
+        
       } else {
         console.log("Image did not hit the target zone");
       }
